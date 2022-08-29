0e32c857df124b3fbc8478ec094658463b755415
Shahmir Varqha <Sham9871@gmail.com>
2022-08-29 03:02:45 +0000

    removed comments

--- src/libs/personaHook.ts
+++ src/libs/personaHook.ts
@@ -1,54 +1,6 @@
 import { useEffect, useState } from "react";
 import { PersonaProps } from "../components/Personas/Persona";
 
-// export const usePersonas = (count?: number) => {
-
-//   const [personas, setPersonas] = useState<PersonaProps[]>([]);
-//   const [loading, setLoading] = useState<boolean>(false);
-//   const [error, setError] = useState<string>("");
-
-//   if(!count) count = 5;
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       setLoading(true)
-//       setPersonas([])
-//       await fetch(`https://fakerapi.it/api/v1/persons?_quantity=${count}`)
-//         .then(res => res.json())
-//         .then(jsonData => {
-//           if(jsonData.code === 200) {
-//             return jsonData.data
-//           }else{
-//             throw new Error(jsonData.code + ": " + jsonData.status)
-//           }
-//         })
-//         .then(persona => {
-//           setPersonas(persona.map((persona : any) : PersonaProps => {
-//             return {
-//               id: persona.id,
-//               firstName: persona.firstName,
-//               lastnName: persona.lastName,
-//               email: persona.email,
-//               gender: persona.gender,
-//               country: persona?.address?.country
-//             }
-//           }))
-//           setLoading(false)
-//         })
-//         .catch(err => setError(err.toString()));
-//     };
-
-//     fetchData();
-
-//   }, [count]);
-
-//   return {
-//     personas,
-//     loading,
-//     error
-//   };
-// }
-
 const usePersonas = (numPersonasToFetch?: number) => {
     const [personas, setPersonas] = useState<PersonaProps[]>([])
     const [loading, setLoading] = useState<boolean>(false);
