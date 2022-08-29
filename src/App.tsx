b66370c35fd5d2247332dacd3045d8a1e5fd737e
Shahmir Varqha <Sham9871@gmail.com>
2022-08-29 02:52:00 +0000

    added main content and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import "./index.css"
+import usePersonas from "./libs/personaHook"
+import PersonaList from "./components/Personas/PersonaList"
 
 function App() {
+  const {personas, loading, error} = usePersonas(5)
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="App">
+      {
+        error ? 
+          (<p>Error: {error} </p>) :
+        loading ? 
+          (<p>Loading data :D</p>) :
+        personas.length > 0 ?
+          (<PersonaList personas={personas} />) :
+          (<p>No personas found, sry</p>)
+      }
+      
+    </div>
   );
 }
 
